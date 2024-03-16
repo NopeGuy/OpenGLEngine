a47aeb61b2a13b17fc1e07e3ac8bc1f29054a805
SenseiBott <jotapastore@gmail.com>
2024-03-16 16:23:31 +0000

    Update parser.cpp

--- fase2/src/utils/parser.cpp
+++ fase2/src/utils/parser.cpp
@@ -8,41 +8,45 @@ struct WindowSettings {
     int height = 0;
 };
 
-struct CameraSettings {
-    float positionX = 0.0f;
-    float positionY = 0.0f;
-    float positionZ = 0.0f;
-    float lookAtX = 0.0f;
-    float lookAtY = 0.0f;
-    float lookAtZ = 0.0f;
-    float upX = 0.0f;
-    float upY = 0.0f;
-    float upZ = 0.0f;
+struct Position {
+    float x = 0.0f;
+    float y = 0.0f;
+    float z = 0.0f;
+};
+
+struct LookAt {
+    float x = 0.0f;
+    float y = 0.0f;
+    float z = 0.0f;
+};
+
+struct Up {
+    float x = 0.0f;
+    float y = 0.0f;
+    float z = 0.0f;
+};
+
+struct Projection {
     float fov = 0.0f;
     float near = 0.0f;
     float far = 0.0f;
+};
+
+struct CameraSettings {
+    Position position;
+    LookAt lookAt;
+    Up up;
+    Projection projection;
 };
 
 struct ModelFile {
     std::string fileName;
 };
 
-struct TransformSettings {
-    float translateX = 0.0f;
-    float translateY = 0.0f;
-    float translateZ = 0.0f;
-};
-
-struct GroupSettings {
-    TransformSettings transform;
-    std::vector<ModelFile> modelFiles;
-    std::vector<GroupSettings> nestedGroups; 
-};
-
 struct ParserSettings {
     WindowSettings window;
     CameraSettings camera;
-    std::vector<GroupSettings> groups;
+    std::vector<ModelFile> modelFiles;
 };
 
 void parseWindowSettings(tinyxml2::XMLElement* windowElement, WindowSettings& window) {
@@ -62,21 +66,21 @@ void parseCameraSettings(tinyxml2::XMLElement* cameraElement, CameraSettings& ca
         auto upElement = cameraElement->FirstChildElement("up");
         auto projectionElement = cameraElement->FirstChildElement("projection");
 
-        positionElement->QueryFloatAttribute("x", &camera.positionX);
-        positionElement->QueryFloatAttribute("y", &camera.positionY);
-        positionElement->QueryFloatAttribute("z", &camera.positionZ);
+        positionElement->QueryFloatAttribute("x", &camera.position.x);
+        positionElement->QueryFloatAttribute("y", &camera.position.y);
+        positionElement->QueryFloatAttribute("z", &camera.position.z);
 
-        lookAtElement->QueryFloatAttribute("x", &camera.lookAtX);
-        lookAtElement->QueryFloatAttribute("y", &camera.lookAtY);
-        lookAtElement->QueryFloatAttribute("z", &camera.lookAtZ);
+        lookAtElement->QueryFloatAttribute("x", &camera.lookAt.x);
+        lookAtElement->QueryFloatAttribute("y", &camera.lookAt.y);
+        lookAtElement->QueryFloatAttribute("z", &camera.lookAt.z);
 
-        upElement->QueryFloatAttribute("x", &camera.upX);
-        upElement->QueryFloatAttribute("y", &camera.upY);
-        upElement->QueryFloatAttribute("z", &camera.upZ);
+        upElement->QueryFloatAttribute("x", &camera.up.x);
+        upElement->QueryFloatAttribute("y", &camera.up.y);
+        upElement->QueryFloatAttribute("z", &camera.up.z);
 
-        projectionElement->QueryFloatAttribute("fov", &camera.fov);
-        projectionElement->QueryFloatAttribute("near", &camera.near);
-        projectionElement->QueryFloatAttribute("far", &camera.far);
+        projectionElement->QueryFloatAttribute("fov", &camera.projection.fov);
+        projectionElement->QueryFloatAttribute("near", &camera.projection.near);
+        projectionElement->QueryFloatAttribute("far", &camera.projection.far);
     }
     else {
         std::cerr << "Camera settings not found." << std::endl;
@@ -99,37 +103,6 @@ void parseModelFiles(tinyxml2::XMLElement* modelsElement, std::vector<ModelFile>
     } else {
         std::cerr << "Model files not found." << std::endl;
         exit(1);
-    }
-}
-
-void parseTransformSettings(tinyxml2::XMLElement* transformElement, TransformSettings& transform) {
-    if (transformElement) {
-        auto translateElement = transformElement->FirstChildElement("translate");
-        translateElement->QueryFloatAttribute("x", &transform.translateX);
-        translateElement->QueryFloatAttribute("y", &transform.translateY);
-        translateElement->QueryFloatAttribute("z", &transform.translateZ);
-    } else {
-        std::cerr << "Transform settings not found." << std::endl;
-        exit(1);
-    }
-}
-
-void parseGroupSettings(tinyxml2::XMLElement* groupElement, std::vector<GroupSettings>& groups) {
-    while (groupElement) {
-        GroupSettings group;
-        tinyxml2::XMLElement* transformElement = groupElement->FirstChildElement("transform");
-        parseTransformSettings(transformElement, group.transform);
-
-        tinyxml2::XMLElement* modelsElement = groupElement->FirstChildElement("models");
-        parseModelFiles(modelsElement, group.modelFiles);
-
-        tinyxml2::XMLElement* nestedGroupElement = groupElement->FirstChildElement("group");
-        if (nestedGroupElement) {
-            parseGroupSettings(nestedGroupElement, group.nestedGroups); 
-        }
-
-        groups.push_back(group);
-        groupElement = groupElement->NextSiblingElement("group");
     }
 }
 
@@ -162,67 +135,13 @@ ParserSettings* ParserSettingsConstructor(const std::string& filePath) {
     parseCameraSettings(cameraElement, settings->camera);
 
     tinyxml2::XMLElement* groupElement = worldElement->FirstChildElement("group");
-    parseGroupSettings(groupElement, settings->groups);
+    if (!groupElement) {
+        std::cerr << "Group element not found." << std::endl;
+        exit(1);
+    }
+
+    tinyxml2::XMLElement* modelsElement = groupElement->FirstChildElement("models");
+    parseModelFiles(modelsElement, settings->modelFiles);
 
     return settings;
-}
-
-void printGroupSettings(const std::vector<GroupSettings>& groups, int level = 0) {
-    for (const auto& group : groups) {
-        std::string indent(level * 2, ' '); // Indentation based on the level of the group
-
-        std::cout << indent << "Transform: (" << group.transform.translateX << ", "
-                  << group.transform.translateY << ", " << group.transform.translateZ << ")\n";
-
-        for (const auto& model : group.modelFiles) {
-            std::cout << indent << "Model File: " << model.fileName << "\n";
-        }
-
-        if (!group.nestedGroups.empty()) {
-            std::cout << indent << "Nested Groups:\n";
-            printGroupSettings(group.nestedGroups, level + 1); // Recursive call for nested groups
-        }
-    }
-}
-
-int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " <xml_file_path>" << std::endl;
-        return 1;
-    }
-
-    const std::string filePath = argv[1];
-
-    // Parse XML file
-    ParserSettings* settings = ParserSettingsConstructor(filePath);
-    if (!settings) {
-        std::cerr << "Failed to parse XML file." << std::endl;
-        return 1;
-    }
-
-    // Print window settings
-    std::cout << "Window Settings:" << std::endl;
-    std::cout << "Width: " << settings->window.width << std::endl;
-    std::cout << "Height: " << settings->window.height << std::endl;
-
-    // Print camera settings
-    std::cout << "\nCamera Settings:" << std::endl;
-    std::cout << "Position: (" << settings->camera.positionX << ", "
-              << settings->camera.positionY << ", " << settings->camera.positionZ << ")" << std::endl;
-    std::cout << "LookAt: (" << settings->camera.lookAtX << ", "
-              << settings->camera.lookAtY << ", " << settings->camera.lookAtZ << ")" << std::endl;
-    std::cout << "Up: (" << settings->camera.upX << ", "
-              << settings->camera.upY << ", " << settings->camera.upZ << ")" << std::endl;
-    std::cout << "FOV: " << settings->camera.fov << std::endl;
-    std::cout << "Near: " << settings->camera.near << std::endl;
-    std::cout << "Far: " << settings->camera.far << std::endl;
-
-    // Print group settings
-    std::cout << "\nGroup Settings:" << std::endl;
-    printGroupSettings(settings->groups);
-
-    // Clean up
-    delete settings;
-
-    return 0;
 }
