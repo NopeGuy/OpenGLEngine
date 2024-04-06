c37fce3a5e06ee71194f0c0e923e5758a258f088
SenseiBott <jotapastore@gmail.com>
2024-04-06 19:48:34 +0100

    *ligmaBalls.3d*

--- fase1/src/utils/list.hpp
+++ fase1/src/utils/list.hpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #ifndef LIST
 #define LIST
 #include <stdlib.h>
@@ -22,4 +23,30 @@ void deleteList(List);
 
 void deepDeleteList(List, void (*)(void *));
 
+=======
+#ifndef LIST
+#define LIST
+#include <stdlib.h>
+
+#define STDSIZE 100
+
+typedef struct list* List;
+
+List newEmptyList();
+
+int listIsFull(List);
+
+void addValueList(List, void*);
+
+void** getListValues(List);
+
+unsigned long getListLength(List);
+
+void* getListElemAt(List, unsigned long);
+
+void deleteList(List);
+
+void deepDeleteList(List, void (*)(void *));
+
+>>>>>>> main
 #endif // LIST
